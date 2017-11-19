2f515381371d3be046aff64a0db5e035032d2570
Timo Hahn <tompeez@gmail.com>
2017-11-19 17:43:24 +0100

    init

---
+++ BBBViewController/src/de/hahn/blog/badgebutton/BadgeButtonBean.java
@@ -0,0 +1,23 @@
+package de.hahn.blog.badgebutton;
+
+import javax.faces.event.ActionEvent;
+
+import oracle.adf.share.logging.ADFLogger;
+
+
+public class BadgeButtonBean {
+    private static ADFLogger logger = ADFLogger.createADFLogger(BadgeButtonBean.class);
+    public BadgeButtonBean() {
+    }
+
+    public void onBadgeButton(ActionEvent actionEvent) {
+       logger.info("Test!");
+            
+    }
+
+
+    public String onBadgeButtonAction() {
+        logger.info("Test5!");
+        return null;
+    }
+}
